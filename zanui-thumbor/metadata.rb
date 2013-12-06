83f8b09d5664257585d09b53039587beba557528
Enrico Stahn <mail@enricostahn.com>
2013-12-06 16:19:44 +1100

    Export from thumbor-box management repository

--- zanui-thumbor/metadata.rb
+++ zanui-thumbor/metadata.rb
@@ -5,5 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures zanui-thumbor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends "apt"
-depends "python"
+
+depends 'apt'
+depends 'python'
